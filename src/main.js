353d0beb6de707f0d2a8238f1912f1f49bb30ef0
danilbakshaev <bakshaevdanil@gmail.com>
2021-01-21 21:12:57 +0500

    страница register и сообщения

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import router from './router'
 import store from './store'
 import dateFilter from '@/filters/date.filter'
 import Vuelidate from 'vuelidate'
+import messagePlugin from '@/utils/message.plugin'
 import 'materialize-css/dist/js/materialize.min'
 
 Vue.config.productionTip = false
 
 Vue.filter('date', dateFilter)
 Vue.use(Vuelidate)
+Vue.use(messagePlugin)
 
 new Vue({
   router,
