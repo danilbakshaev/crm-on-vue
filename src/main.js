6578d39e68f164120462be0ea29f0848ad6f56a7
danilbakshaev <bakshaevdanil@gmail.com>
2021-02-10 12:58:26 +0500

    Добавил переключение языков

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import store from './store'
 import dateFilter from '@/filters/date.filter'
 import currencyFilter from './filters/currency.filter'
+import localizeFilter from './filters/localize.filter'
 import tooltipDirective from '@/directives/tooltip.directive'
 import Vuelidate from 'vuelidate'
 import messagePlugin from '@/utils/message.plugin'
@@ -13,14 +14,14 @@ import 'materialize-css/dist/js/materialize.min'
 import Loader from '@/components/app/Loader'
 
 import firebase from 'firebase/app'
-import 'firebase/auth';
-import 'firebase/database';
-
+import 'firebase/auth'
+import 'firebase/database'
 
 Vue.config.productionTip = false
 
 Vue.filter('date', dateFilter)
 Vue.filter('currency', currencyFilter)
+Vue.filter('localize', localizeFilter)
 Vue.use(Vuelidate)
 Vue.use(messagePlugin)
 Vue.directive('tooltip', tooltipDirective)
@@ -28,13 +29,13 @@ Vue.component('Loader', Loader)
 Vue.component('Paginate', Paginate)
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCqO0_-0KKhdcNmgGMHjuWlwj1EqbFe2vM",
-  authDomain: "crm-vue-ae010.firebaseapp.com",
-  projectId: "crm-vue-ae010",
-  storageBucket: "crm-vue-ae010.appspot.com",
-  messagingSenderId: "342719283033",
-  appId: "1:342719283033:web:5611bd4dff7f5a0c4c1648",
-  measurementId: "G-SQQBPMJFC2"
+  apiKey: 'AIzaSyCqO0_-0KKhdcNmgGMHjuWlwj1EqbFe2vM',
+  authDomain: 'crm-vue-ae010.firebaseapp.com',
+  projectId: 'crm-vue-ae010',
+  storageBucket: 'crm-vue-ae010.appspot.com',
+  messagingSenderId: '342719283033',
+  appId: '1:342719283033:web:5611bd4dff7f5a0c4c1648',
+  measurementId: 'G-SQQBPMJFC2'
 })
 
 let app
